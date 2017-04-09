f9f34968df754e81c5fcdda682bbe8f61b155312
vartankoko <vartan.koko@gmail.com>
2017-04-09 22:42:16 +0200

    Fichier de test de commentaires

---
+++ src/test/java/com/example/CommentTest.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * Created by kokoghlanian on 09/04/2017.
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CommentTest {
+
+}
