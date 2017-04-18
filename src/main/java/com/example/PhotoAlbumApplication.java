6635dc09cedf5d62d739f13ae25c9cfaf83a7e69
Yukenigon <timothee.arnauld1@gmail.com>
2017-04-18 20:52:51 +0200

    Change server port

--- src/main/java/com/example/PhotoAlbumApplication.java
+++ src/main/java/com/example/PhotoAlbumApplication.java
@@ -1,8 +1,13 @@
 package com.example;
 
+import org.neo4j.ogm.config.Configuration;
+import org.neo4j.ogm.session.SessionFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
+import org.springframework.data.neo4j.transaction.Neo4jTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @org.springframework.context.annotation.Configuration
